81d587d66e51d1e47c42cd382b51729e45358b89
Ricardo Gladwell <ricardo@gladwell.me>
2020-02-19 21:13:16 +0000

    De-duplicate mixin

--- wcs-microdata.js
+++ wcs-microdata.js
@@ -1,36 +1,41 @@
 import '../microtesia.js/lib/microtesia.js'
+import { dedupeMixin } from '@open-wc/dedupe-mixin'
 
-export const MicrodataMixin = (base) => class extends base {
+export const MicrodataMixin = dedupeMixin(
+  base =>
+    class extends base {
 
-  connectedCallback() {
-    if (super.connectedCallback) super.connectedCallback()
-    this.__updateMicrodata()
-  }
-
-  performUpdate() {
-    this.__updateMicrodata()
-    if (super.performUpdate) super.performUpdate()
-  }
-
-  __updateMicrodata() {
-    function filterProperty(property, value) {
-      if ((property === Array || property.type === Array) && !Array.isArray(value)) {
-        return [value]
-      } else {
-        return value
+      connectedCallback() {
+        if (super.connectedCallback) super.connectedCallback()
+        this.__updateMicrodata()
       }
+
+      performUpdate() {
+        this.__updateMicrodata()
+        if (super.performUpdate) super.performUpdate()
+      }
+
+      __updateMicrodata() {
+        function filterProperty(property, value) {
+          if ((property === Array || property.type === Array) && !Array.isArray(value)) {
+            return [value]
+          } else {
+            return value
+          }
+        }
+
+        const microdata = parseMicrodata(this)[0]
+
+        for (const name in microdata) {
+          const value = microdata[name]
+          const property = this.constructor.properties[name]
+
+          if (property) {
+            this[name] = filterProperty(property, value)
+          }
+        }
+      }
+
     }
 
-    const microdata = parseMicrodata(this)[0]
-
-    for (const name in microdata) {
-      const value = microdata[name]
-      const property = this.constructor.properties[name]
-
-      if (property) {
-        this[name] = filterProperty(property, value)
-      }
-    }
-  }
-
-}
+)
